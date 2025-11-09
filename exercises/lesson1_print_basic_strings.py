7f1d1c30b1981226e13bfca097eb8b9bce9921f3
michael.golov <michael.golov@cyberark.com>
2025-11-09 18:11:38 -0500

    python lesson 1

---
+++ exercises/lesson1_print_basic_strings.py
@@ -0,0 +1,11 @@
+#1. Print a short story using multiple lines.
+print("Hi my name is Mike, and I live in Boston. I love to watch sports")
+
+#2. Create a string with includes single and double quotes.
+print("Python is a fun programming language. I keep saying 'This course is awesome'")
+
+#3. Use escape characters to create a formatted list.
+print("Current standings:\nThe\n\tNew\n\t\tEngland\n\t\t\tPatriots\n\t\t\t\tare\n\t\t\t\t\tin\n\t\t\t\t\t\tfirst\n\t\t\t\t\t\t\tplace")
+
+#4. Combine different types of data (string and numbers) in one print statement
+print("The Patriots scored", 28, "points today")
