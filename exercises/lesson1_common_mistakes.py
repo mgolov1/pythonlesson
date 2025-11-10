fa743a0ec65a2f1f32c497f011aa71547102921f
michael.golov <michael.golov@cyberark.com>
2025-11-09 19:14:23 -0500

    common mistakes lesson

---
+++ exercises/lesson1_common_mistakes.py
@@ -0,0 +1,6 @@
+print('Let\'s practice finding errors!')
+print('Let\'s practice finding errors!')
+print('What\'s wrong with this line?')
+print('Fix me')
+print('What is missing here?')
+print("What do I use" + " " + "incorrectly here?")
