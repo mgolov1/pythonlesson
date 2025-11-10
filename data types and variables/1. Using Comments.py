f8815758b2a81f3f113731146f7da029b67c64ed
michael.golov <michael.golov@cyberark.com>
2025-11-09 20:39:07 -0500

    using comments

---
+++ data types and variables/1. Using Comments.py
@@ -0,0 +1,35 @@
+# This is a single-line comment
+x = 10  # Assign the value of 10 to the variable x
+y = 20  # Assign the value of 20 to the variable y
+
+# Calculate the sum of x = y
+result = x + y
+print(result)
+
+# This is a multi-line comment
+# created by using multiple
+# single-line comments.
+
+"""
+This is a multi-line comment
+created by using string literals using double-quotes.
+"""
+
+'''
+This is a multi-line comment
+created by using string literals using single-quotes.
+'''
+
+# Commented out code
+x = 10  # Assign the value of 10 to the variable x
+y = 20  # Assign the value of 20 to the variable y
+
+# Calculate the sum of x = y
+result = x + y
+print(result)
+
+# PyCharm shortcuts
+# Ctrl +/
+# Shift + Alt +. to make the font size larger
+# Shift + Alt +, to make the font size smaller
+# Ctrl d, to duplicate the line
