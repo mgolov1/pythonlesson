0e5f2ec1c2dc7c7463a3589faa132e83744b43f9
michael.golov <michael.golov@cyberark.com>
2025-11-10 09:48:37 -0500

    learning data types

---
+++ data types and variables/2. Data Types.py
@@ -0,0 +1,18 @@
+# Understanding Data Types in Python
+# Common Data Types
+# Numbers
+3, 6, 9  #Integers (int)
+3.14, -0.12  #Floats (float)
+# Text
+'This is a string'  #String (str)
+# Boolean
+True, False #  Boolean (bool)
+
+age = 27 # Age is an integer (int)
+p = 3.14 # Pi is a float (float)
+name = 'Mike' # Name is a string (str)
+is_new = True # is_new is a boolean (bool)
+
+# Check data type
+# type()
+print(type(age)) # check 'age' variable with type() function
