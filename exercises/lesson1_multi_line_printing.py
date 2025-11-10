5352daa9c6a16ea9cc011fb71527eac53441fe83
michael.golov <michael.golov@cyberark.com>
2025-11-09 19:33:57 -0500

    multiline printing

---
+++ exercises/lesson1_multi_line_printing.py
@@ -0,0 +1,25 @@
+# Try printing a simple three-line text using three separate print() statements.
+print("This is the first line,")
+print("This is the second line,")
+print("This is the third line")
+
+print("\n")
+
+# Try printing a simple three-line text using one print() statement with \n.
+print("This is the first line,\nThis is the second line,\nThis is the third line")
+
+print("\n")
+
+# Print it using triple quotes.
+print("""This is the first line,
+This is the second line,
+This is the third line""")
+
+print("\n")
+
+# Add a blank line between each string of your text using any method you prefer.
+print("""This is the first line,
+
+This is the second line,
+
+This is the third line""")
