d32a33a5636af4cd7ee6a89162fcc22f37def74c
michael.golov <michael.golov@cyberark.com>
2025-11-20 21:08:03 -0500

    more coding

--- exercises/lesson2_strings_manipulation.py
+++ exercises/lesson2_strings_manipulation.py
@@ -5,16 +5,16 @@ first_name = "Mike"
 last_name = "Golov"
 
 # 3. Create a variable full_name by concatenating first_name and last_name with a space inbetween.
-full_name = f"{first_name} {last_name}"
+full_name = first_name + " " + last_name
 
 # 4. Print out the full_name
-print(full_name)
+print(f"Full Name: {full_name}")
 
 # 5. Print out the length of full_name using the len() function.
-print(len(full_name))
+print(f"Length of Full Name: {len(full_name)}")
 
 # 6. Print out the full_name in uppercase letters.
-print(full_name.upper())
+print(f"Uppercase: {full_name.upper()}")
 
 # 7. Print out the full_name in all lowercase letters.
-print(full_name.lower())
+print(f"Lowercase: {full_name.lower()}")
