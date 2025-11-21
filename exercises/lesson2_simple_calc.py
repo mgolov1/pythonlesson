2675da12802226e4ba3a6e96b0ff378db73e9697
michael.golov <michael.golov@cyberark.com>
2025-11-20 21:03:56 -0500

    more coding

---
+++ exercises/lesson2_simple_calc.py
@@ -0,0 +1,17 @@
+# 1. Create two variables, num1 and num2, and assign them integer values.
+num1 = 10
+num2 = 2
+
+# 2. Perform the following operations and print the results.
+# Addition of num1 and num2
+print(f"Additional: {num1 + num2}")
+# Subtraction of num1 and num2
+print(f"Subtraction: {num1 - num2}")
+# Multiplication of num1 and num2
+print(f"Multiplication: {num1 * num2}")
+# Division of num1 and num2
+print(f"Division: {num1 / num2}")
+# Integer division of num1 and num2
+print(f"Integer division: {num1 // num2}")
+# Remainder of num1 divided by num2
+print(f"Remainder: {num1 % num2}")
---
+++ exercises/lesson2_strings_manipulation.py
@@ -0,0 +1,20 @@
+# 1. Create a variable first_name and assign it to your first name.
+first_name = "Mike"
+
+# 2. Create a variable last_name and assign it to your last name.
+last_name = "Golov"
+
+# 3. Create a variable full_name by concatenating first_name and last_name with a space inbetween.
+full_name = f"{first_name} {last_name}"
+
+# 4. Print out the full_name
+print(full_name)
+
+# 5. Print out the length of full_name using the len() function.
+print(len(full_name))
+
+# 6. Print out the full_name in uppercase letters.
+print(full_name.upper())
+
+# 7. Print out the full_name in all lowercase letters.
+print(full_name.lower())
