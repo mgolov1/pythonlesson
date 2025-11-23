dcb67eaff478b2b7014794008af4b963e97e7f15
michael.golov <michael.golov@cyberark.com>
2025-11-22 20:18:27 -0500

    more coding

---
+++ exercises/lesson6_user_input_exercises.py
@@ -0,0 +1,17 @@
+# 1. Greeting
+# Create a simple program that asks for users name and responds with personalized greeting.
+
+age = input("Enter your age: ")
+print("You are " + age + " years old")
+
+# 2. Echo Machine
+# Create a simple program that prompts the user to type something and then repeat it back exactly as entered.
+
+user_input = input("Say something: ")
+print(user_input)
+
+# 3. Favorite Color
+# Create a program that inquires about the users favorite color and responds with the comment about that color.
+
+color = input("What's your favorite color: ")
+print(f"{color} is a great color! It reminds me of {color} skies.")
