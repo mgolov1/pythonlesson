1cb4e4347df13a680e1e37e16c0c2bc7548dcb7d
michael.golov <michael.golov@cyberark.com>
2025-11-23 18:18:31 -0500

    more coding

--- exercises/demo.py
+++ exercises/demo.py
@@ -45,3 +45,62 @@ complete_inventory = laptop_inventory + tablet_inventory
 branch_setups = tablet_inventory * 3
 total_tablet_inventory = len(tablet_inventory + branch_setups)
 print(total_tablet_inventory)
+
+employees = [
+    ["Alice", "IT", 75000],
+    ["Bob", "HR", 65000],
+    ["Carol", "IT", 78000]
+]
+
+it_salaries = [emp[2] for emp in employees if emp[1] == "IT"]
+print(it_salaries)
+
+average_it_salary = sum(it_salaries) / len(it_salaries)
+print(f"Average IT Salary: ${average_it_salary}")
+
+departments = set ([emp[1] for emp in employees])
+print(departments)
+
+employee_data = [
+    [1001, 50000, 'HR', 'Active'],
+    [1002, 60000, 'IT', 'Active'],
+    [1004, 65000, 'IT', 'Active']
+]
+
+adjusted_salaries = [employee[1] + 5000 for employee in employee_data]
+print(adjusted_salaries)
+it_department = [employee[1] for employee in employee_data]
+print(it_department)
+active_employee_ids = [employee[0] for employee in employee_data if employee[3] == 'Active']
+print(active_employee_ids)
+department_codes = [employee[2] for employee in employee_data]
+formatted_codes = [code.lower() for code in department_codes]
+print(formatted_codes)
+high_salary_employees = [employee for employee in employee_data if employee[1] > 55000]
+print(high_salary_employees)
+
+sales_data = [
+    [100, 150, 200], # Product A daily sales
+    [300, 350, 400], # Product B daily sales
+    [500, 550, 600] # Product C daily sales
+]
+
+daily_totals = (sum(day) for day in sales_data)
+print(f"Day 1 total sales: ${next(daily_totals)}")
+print(f"Day 2 total sales: ${next(daily_totals)}")
+print(f"Day 3 total sales: ${next(daily_totals)}")
+
+product_totals = list(map(sum, sales_data))
+print(product_totals)
+
+# {sale for product in sales_data for sale in product}
+
+unique_sales = set()
+for product in sales_data:
+    for sale in product:
+        unique_sales.add(sale)
+print("Unique sales value:", sorted (unique_sales, reverse=True))
+daily_increases = [day2 - day1 for day1, day2 in zip(sales_data[0][:-1], sales_data[0][1:])]
+print(daily_increases)
+for increase in daily_increases:
+    print(f"Increase: ${increase}")
